39fa1ddc76d8b343467f9cf958bb4de458c64db2
eduardobazler <edubazler@gmail.com>
2021-11-18 21:04:34 -0300

    segundo exercicio

--- fundamentos/bloco-5-javaScript-DOM-Eventos-webStorage/dia-3-eventos/exercicios/script.js
+++ fundamentos/bloco-5-javaScript-DOM-Eventos-webStorage/dia-3-eventos/exercicios/script.js
@@ -1,19 +1,19 @@
 function createDaysOfTheWeek() {
     const weekDays = ['Domingo', 'Segunda', 'Terça', 'Quarta', 'Quinta', 'Sexta', 'Sábado']
     const weekDaysList = document.querySelector('.week-days');
-  
+
     for (let index = 0; index < weekDays.length; index += 1) {
-      const days = weekDays[index];
-      const dayListItem = document.createElement('li');
-      dayListItem.innerHTML = days;
-  
-      weekDaysList.appendChild(dayListItem);
+        const days = weekDays[index];
+        const dayListItem = document.createElement('li');
+        dayListItem.innerHTML = days;
+
+        weekDaysList.appendChild(dayListItem);
     };
-  };
-  
-  createDaysOfTheWeek();
-  
-  // Escreva seu código abaixo.
+};
+
+createDaysOfTheWeek();
+
+// Escreva seu código abaixo.
 
 //   Exercício 1:
 
@@ -21,18 +21,18 @@ const dezDaysList = [29, 30, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 
 
 let listaPai = document.querySelector('#days');
 
-for (let i = 0; i < dezDaysList.length; i++){
+for (let i = 0; i < dezDaysList.length; i++) {
 
     let day = document.createElement('li');
 
     day.innerText = dezDaysList[i];
     day.classList.add('day');
-    
-    if(dezDaysList[i] === 24 || dezDaysList[i] === 25 || dezDaysList[i] === 31){
+
+    if (dezDaysList[i] === 24 || dezDaysList[i] === 25 || dezDaysList[i] === 31) {
         day.classList.add('holiday');
     }
 
-    if(dezDaysList[i] === 4 || dezDaysList[i] === 11 || dezDaysList[i] === 18 || dezDaysList[i] === 25){
+    if (dezDaysList[i] === 4 || dezDaysList[i] === 11 || dezDaysList[i] === 18 || dezDaysList[i] === 25) {
         day.classList.add('friday');
     }
 
@@ -40,3 +40,20 @@ for (let i = 0; i < dezDaysList.length; i++){
 
 }
 
+//2
+
+
+function exercicio2 (string){
+
+    let paiExc2 = document.querySelector('.buttons-container');
+
+    let botao = document.createElement('button');
+
+    botao.innerText = string;
+
+    botao.id = 'btn-holiday'
+
+    paiExc2.appendChild(botao);
+} 
+
+exercicio2('Feriados');
