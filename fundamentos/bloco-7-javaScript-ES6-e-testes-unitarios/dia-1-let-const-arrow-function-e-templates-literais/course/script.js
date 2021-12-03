2f2043f34dceac08de0594d3c1ee5d939ea90e17
eduardobazler <edubazler@gmail.com>
2021-12-03 18:51:37 -0300

    atualizando repositório

---
+++ fundamentos/bloco-7-javaScript-ES6-e-testes-unitarios/dia-1-let-const-arrow-function-e-templates-literais/course/script.js
@@ -0,0 +1,28 @@
+/* function soma (num1, num2) {
+  return num1 + num2;
+} */
+
+/* const soma = (num1, num2) => {
+  return num1 + num2;
+}; */
+
+/* const soma = (num1, num2) => num1 + num2;
+
+console.log(soma(2, 4)); */
+
+/* function contaPalavras (frase) {
+  return frase.split(' ').length;
+} */
+
+/* const contaPalavras = (frase) => frase.split(' ').length;
+ */
+
+/* console.log(contaPalavras('Fala tribo, beleza?')); */
+
+function objetoPessoa (nome, idade) {
+  return {
+    nome: nome,
+    idade: idade
+  }
+}
+
