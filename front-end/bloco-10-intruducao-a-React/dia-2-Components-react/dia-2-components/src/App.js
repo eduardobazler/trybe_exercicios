5b5ab0f18722d5feea7c09e50dcb5c7e23c9c93e
eduardobazler <edubazler@gmail.com>
2022-01-24 18:48:18 -0300

    adicionando course do dia

---
+++ front-end/bloco-10-intruducao-a-React/dia-2-Components-react/dia-2-components/src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+import Greeting from './Greeting';
+import Image from './Image';
+import Composicao from './Compisicao';
+import LandingPage from './LandingPage';
+
+function App() {
+  return (
+    <div className="App">
+      <Greeting/>
+    </div>
+  );
+}
+
+export default App;
