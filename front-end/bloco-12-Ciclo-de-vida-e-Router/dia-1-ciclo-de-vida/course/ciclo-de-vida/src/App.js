cc185ba7f524f5b437b94917d1767b0bf678fe35
eduardobazler <edubazler@gmail.com>
2022-02-02 14:47:44 -0300

    atualizando repositório

---
+++ front-end/bloco-12-Ciclo-de-vida-e-Router/dia-1-ciclo-de-vida/course/ciclo-de-vida/src/App.js
@@ -0,0 +1,16 @@
+// App.js
+import React, { Component }from 'react';
+import './App.css';
+import DadJoke from './DadJoke';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <DadJoke />
+      </div>
+    );
+  }
+}
+
+export default App;
