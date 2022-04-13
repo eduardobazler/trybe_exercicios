52d0b5a3dfe85ccc82334b75d938b990fad619a6
eduardobazler <edubazler@gmail.com>
2022-04-13 06:16:20 -0300

    atualizando repositório

--- front-end/bloco-15-Redux/teste-com-redux/testes-react-redux/src/App.test.js
+++ front-end/bloco-15-Redux/teste-com-redux/testes-react-redux/src/App.test.js
@@ -1,14 +1,19 @@
 import React from 'react'
 import { Provider } from 'react-redux'
-import { render, cleanup } from '@testing-library/react';
+import { render, cleanup, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event'
 import App from './App';
 // demais imports...
 import { createStore, combineReducers } from 'redux';
 import clickReducer from './reducers';
 
+const createMockStroe = (initialState) => (
+  createStore(combineReducers({ clickReducer }), initialState)
+)
+
 const renderWithRedux = (
   component,
-  { initialState, store = createStore(combineReducers({ clickReducer }), initialState) } = {}
+  { initialState, store = createMockStroe(initialState) } = {}
 ) => {
   return {
     ...render(<Provider store={store}>{component}</Provider>),
@@ -19,10 +24,26 @@ const renderWithRedux = (
 describe('testing clicks', () => {
   beforeEach(cleanup);
   test('the page should has a button and a text 0', () => {
-    const { queryByText } = renderWithRedux(<App />, { initialState: { clickReducer: { counter: 5 }}});
-    const buttonAdicionar = queryByText('Clique aqui');
+    renderWithRedux(<App />);
+    const buttonAdicionar = screen.queryByText('Clique aqui');
 
     expect(buttonAdicionar).toBeInTheDocument();
-    expect(queryByText('0')).toBeInTheDocument();
+    expect( screen.getByText('0')).toBeInTheDocument();
+  });
+  test('a click in a button should increment the value of clicks', () => {
+   renderWithRedux(<App />, { initialState: { clickReducer: { counter: 5 }}});
+
+    expect(screen.getByText('5')).toBeInTheDocument();
+  });
+  test('the page should has a button and after click a text', () => {
+    renderWithRedux(<App />);
+    const buttonAdicionar = screen.queryByText('Clique aqui');
+
+    expect(buttonAdicionar).toBeInTheDocument();
+    expect( screen.getByText('0')).toBeInTheDocument();
+
+    userEvent.click(buttonAdicionar);
+    expect( screen.getByText('1')).toBeInTheDocument();
+    
   });
 });
